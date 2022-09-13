e9ab24bd44537b547c2cdc24d5d894a82bdada43
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2022-09-13 09:43:49 +0000

    MY resource is great

---
+++ main.py
@@ -0,0 +1,13 @@
+class border:
+  def __init__(self,home,school):
+    self._home = home
+    self._school = school
+
+  def sch(self):
+    return self._school
+  
+  def hom(self):
+    return self._home
+  
+
+ample = border('Ramseyer','KNUST')
